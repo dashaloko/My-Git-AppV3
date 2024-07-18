5430aa5acaf4eb7b907976341ecee6659ac00074
Daria <dashalokomoko30@gmail.com>
2024-07-18 09:02:44 -0400

    Add app version

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Calculator app.");
+        System.out.println("My git app. Super!");
+        System.out.println("Version 1.0");
     }
 }
